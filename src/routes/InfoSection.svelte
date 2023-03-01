e10e407c55ca6d7971651ef216679362cff1b31a
Robbin Schepers <robbin@deschepers.nl>
2023-03-01 13:53:33 +0100

    fix: Moved around info section block and added a background

--- src/routes/InfoSection.svelte
+++ src/routes/InfoSection.svelte
@@ -2,18 +2,21 @@
 	import TagBlock from '$lib/components/TagBlock.svelte';
 </script>
 
-<div class="bg-white text-black py-10 w-full max-w-5xl m-auto" id="info">
-	<TagBlock class="max-w-md float-right" direction="left" extend="true">
-		April 13th Ordina is organizing FRONTEND FESTIVAL. An event with international speakers and
-		workshops, free of charge! You can expect new insights, lots of delicious snacks, a cozy
-		atmosphere and more…
-	</TagBlock>
-	<TagBlock
-		class="max-w-md float-left mt-5 lg:mt-20"
-		direction="right"
-		backgroundColor="shark"
-		extend="true"
-	>
-		Are you a frontend developer who is ‘in’ for inspiration? Join us and get your ticket now!
-	</TagBlock>
+<div class="w-full py-10 bg-western text-black">
+	<div class="text-xl w-full max-w-6xl m-auto" id="info">
+		<TagBlock class="max-w-lg float-left" direction="right" extend="true">
+			April 13th Ordina is organizing FRONTEND FESTIVAL. An event with international speakers and
+			workshops, free of charge! You can expect new insights, lots of delicious snacks, a cozy
+			atmosphere and more…
+		</TagBlock>
+		<TagBlock
+			class="max-w-lg float-right mt-5 lg:mt-32"
+			direction="left"
+			backgroundColor="shark"
+			extend="true"
+		>
+			Are you a frontend developer who is ‘in’ for inspiration? Join us and get your ticket now!
+		</TagBlock>
+	</div>
+	<div class="clear-both" />
 </div>
