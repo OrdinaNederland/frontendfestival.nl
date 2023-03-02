8e5b7f0102c4f9bad19e4a31f4b5dc7fed5156f5
Robbin Schepers <robbin@deschepers.nl>
2023-03-02 16:11:09 +0100

    fix: Created more space in the header

--- src/routes/HeaderSection.svelte
+++ src/routes/HeaderSection.svelte
@@ -8,7 +8,7 @@
 <header class="flex-center w-full h-[85vh] bg-shark relative" id="header">
 	<Poster />
 	<div class="z-50 flex flex-col justify-center max-h-full text-center">
-		<h1 class="block w-screen max-w-3xl p-10">
+		<h1 class="block w-screen max-w-4xl p-20">
 			<a href="https://frontendfestival.nl">
 				<img src="/logo.svg" alt="Frontend Festival Logo" class="" />
 			</a>
@@ -19,9 +19,9 @@
 			rel="noreferrer"
 			href="https://www.eventbrite.com/e/tickets-frontend-festival-522544092447"
 		>
-			<TagBlock class="text-xl" backgroundColor="fountain"
-				>tickets.getYoursFor<span class="font-bold">Free</span>();</TagBlock
-			>
+			<TagBlock class="text-xl" backgroundColor="fountain">
+				tickets.getYoursFor<span class="font-bold">Free</span>();
+			</TagBlock>
 		</a>
 		<div class="absolute w-full bottom-0 left-0 flex-center">
 			<a class="inline mb-10" href="#info">
