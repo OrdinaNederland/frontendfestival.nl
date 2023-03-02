dea4b8463c4d56b9b5a15b71601d0eca59a3d1e9
Robbin Schepers <robbin@deschepers.nl>
2023-03-02 17:42:28 +0100

    fix: Info section scrolldown is now smooth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="flex h-screen w-screen flex-col overflow-x-hidden">
-	<div class="min-h-screen overflow-x-hidden">
+	<div class="min-h-screen overflow-x-hidden scroll-smooth">
 		<slot />
 	</div>
 </div>
