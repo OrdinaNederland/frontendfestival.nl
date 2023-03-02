abe3b262fbade48a163f0969b22cf9d0ac92323f
Robbin Schepers <robbin@deschepers.nl>
2023-03-02 17:46:20 +0100

    fix: Removed id from the spacer

--- src/routes/Spacer.svelte
+++ src/routes/Spacer.svelte
@@ -2,6 +2,6 @@
 	import Poster from '$lib/components/Poster.svelte';
 </script>
 
-<header class="flex-center relative h-10 w-full bg-shark md:h-48" id="header">
+<header class="flex-center relative h-10 w-full bg-shark md:h-48">
 	<Poster colors={['#232728', '#232728', '#FF8133']} />
 </header>
