c1dede2a975ffc6f74c24b74174bdca4eb5bc76c
Robbin Schepers <robbin@deschepers.nl>
2023-03-02 16:24:57 +0100

    fix: Fixed spacer being too much on mobile

--- src/routes/Spacer.svelte
+++ src/routes/Spacer.svelte
@@ -2,6 +2,6 @@
 	import Poster from '$lib/components/Poster.svelte';
 </script>
 
-<header class="flex-center w-full h-48 bg-shark relative" id="header">
+<header class="flex-center w-full h-10 md:h-48 bg-shark relative" id="header">
 	<Poster colors={['#232728', '#232728', '#FF8133']} />
 </header>
