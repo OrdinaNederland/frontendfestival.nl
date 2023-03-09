1c9c0ba31949fc8c23e20555ae29c9e415f755dd
Robbin Schepers <robbin@deschepers.nl>
2023-03-09 15:44:56 +0100

    feat: Added og-meta to the homepage for socials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,10 +10,21 @@
 
 <svelte:head>
 	<title>Frontend Festival 2023</title>
+
+	<meta name="title" property="og:title" content="Frontend Festival 2023" />
 	<meta
 		name="description"
-		content="Let's talk about Frontend: tijdens het Frontend festival nemen we je mee in interessante onderwerpen over Frontend"
+		property="og:description"
+		content="Are you a frontend developer who is ‘in’ for inspiration? Join us and get your ticket now!"
 	/>
+	<meta name="type" property="og:type" content="website" />
+	<meta name="url" property="og:url" content="https://frontendfestival.nl/" />
+	<meta
+		name="image"
+		property="og:image"
+		content="https://frontendfestival.nl/marketing/socials.png"
+	/>
+	<meta name="twitter:card" content="summary_large_image" />
 </svelte:head>
 
 <HeaderSection />
